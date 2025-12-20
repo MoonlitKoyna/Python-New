31ae639e422083b599322ae8f255013f2b061be2
MoonlitKoyna <koynauwu@gmail.com>
2025-12-20 13:47:45 +0530

    Add file handling examples and update student records

---
+++ AfterclassProject-2/Main.py
@@ -0,0 +1,12 @@
+file = open("Example.txt", "r")
+print(file.read())
+file.close()
+
+file = open("Example.txt", "r")
+print("\n Reading")
+print(file.read(7))
+file.close()
+
+file = open("Example.txt", "a")
+file.write(" \n Hiiiii this is Koyna!")
+file.close()
