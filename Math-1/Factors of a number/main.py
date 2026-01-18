4a19440a2e81e38998c56517eb6ab137aa21d7d4
MoonlitKoyna <koynauwu@gmail.com>
2026-01-18 15:26:48 +0530

    Add function to print factors of a given number

---
+++ Math-1/Factors of a number/main.py
@@ -0,0 +1,7 @@
+def print_factors(number):
+    print("The Factors of ", number, "are:")
+    for i in range(1, number+1):
+        if number % i==0:
+            print(i)
+number= int(input("Enter a number:"))
+print_factors(number)
