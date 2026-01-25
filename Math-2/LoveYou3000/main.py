4e8a0162101eb8b4b5e4c92ff745d284e1c79180
MoonlitKoyna <koynauwu@gmail.com>
2026-01-25 17:32:41 +0530

    Remove outdated implementation of palindromic prime number finder and add request for code improvement suggestions

--- Math-2/LoveYou3000/main.py
+++ Math-2/LoveYou3000/main.py
@@ -9,6 +9,6 @@ for num in range (1, a+1):
     if c==2:
         while temp>0:
             rev = rev*10 + (temp%10)
-            temp//=10
+            temp//10
         if rev == num:
             print(num)
