9700535dec1d3a4708118ba1bf14801b17baeabb
MoonlitKoyna <koynauwu@gmail.com>
2026-01-31 15:40:06 +0530

    Add initial implementations for bitwise operations, number of bits, and odd/even bitwise checks

---
+++ Bit Manipulation/ImplemenBitwise/main.py
@@ -0,0 +1,32 @@
+num1=10
+num2=4
+
+# Bitwise AND operation
+print("num1 & num2 =", num1 & num2)
+
+# Bitwise OR operation
+print("\n num1 | num2 =", num1 | num2)
+
+# Bitwise NOT operation
+print("\n~num1 =", ~num1)
+
+# Bitwise XOR operation
+print("\n num1 ^ num2= ", num1 ^ num2)
+
+num1=10
+num2=4
+
+# Bitwise right shift operation
+print("\n num1 >> 1 =", num1 >> 1)
+
+# Bitwise left shift operation
+print("\n num2 << 1 = ", num2 << 1)
+
+num1=10
+num2=4
+
+#Using left shift operator on num1
+print("\n num1 << 1 =", num1 << 1)
+
+#Using right shift operator on num2
+print(" \n num2 << 1=", num2 >> 1)
